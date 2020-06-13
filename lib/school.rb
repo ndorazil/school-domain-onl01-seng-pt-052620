605513eca646c7b6cb2f6fe5991f829569f65354
ndorazil <nathandorazil@live.com>
2020-06-12 21:23:32 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+    attr_reader :name
+
+    ROSTER = {}
+
+    def initialize(name)
+        @name = name
+        @ROSTER = {}
+    end
+
+    def initialize(student, age)
+        @student = student
+        @age = age
+        ROSTER << student
+    end
+
+    def roster
+        @ROSTER
+    end
+end
